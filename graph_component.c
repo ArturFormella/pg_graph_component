58121e401dab5b640b16da3551f01d7a2358a38c
artur <a.formella3@gmail.com>
2024-07-27 23:44:32 +0000

    Fixed coding style

--- graph_component.c
+++ graph_component.c
@@ -1,5 +1,5 @@
 /*
-Author: Artur Formella
+* Author: Artur Formella
 */
 
 #include <postgres.h>
@@ -48,7 +48,7 @@ void copyLinkedListToStr(Vertex *arr, StringInfo string, int deep) {
     if (arr->nextItem) {
       appendStringInfoString(string, ",");
       copyLinkedListToStr(arr->nextItem, string, deep+1);
-    } 
+    }
   }
 }
 
@@ -72,14 +72,6 @@ void mergeToFirst(Vertex* A, Vertex* B) {
   StringInfo Astring = makeStringInfo();
   copyLinkedListToStr(A->firstItem, Astring, 1);
 
-  StringInfo Bstring = makeStringInfo();
-  copyLinkedListToStr(A->firstItem, Bstring, 1);
-
-  StringInfo LLstring = makeStringInfo();
-  copyLinkedListToStr(A->firstItem, LLstring, 1);
-
-  StringInfo RRstring = makeStringInfo();
-  copyLinkedListToStr(A->firstItem, RRstring, 1);
 */
 /*
   if (LL == RR && LL->id != RR->id) {
@@ -119,10 +111,10 @@ void mergeToFirst(Vertex* A, Vertex* B) {
 
       /*
     if (A->firstItem->nextItem == B) {
-      elog(ERROR, "er (%d, %d)\n(A %s)\n(B %s)\nLL  %d first: %d(%d) last %d \nRR %d first: %d(%d) last %d\nLAST: (%d, %d)\nFIRSTLAST: (%d, %d)", 
+      elog(ERROR, "er (%d, %d)\n(A %s)\n(B %s)\nLL  %d first: %d(%d) last %d \nRR %d first: %d(%d) last %d\nLAST: (%d, %d)\nFIRSTLAST: (%d, %d)",
 
-        A->id,   
-        B->id,   
+        A->id,
+        B->id,
 
         Astring->data,
         Bstring->data,
@@ -166,7 +158,7 @@ void mergeToFirst(Vertex* A, Vertex* B) {
           Bstring->data,
           LLstring->data,
           RRstring->data
-        ); 
+        );
       }
       if (prevItem == LLNext) {    elog(ERROR, "prevItem = LLNext");      }
       if (prevItem == NULL) {        elog(ERROR, "prevItem NULL");      }
@@ -183,8 +175,8 @@ void mergeToFirst(Vertex* A, Vertex* B) {
         prevItem = RRNext;
 /*
         if (RRNext == RRNext->nextItem) {     //     elog(ERROR, "RRNext = RRNext");        }
-        if (prevItem->nextItem == RRNext->nextItem && prevItem->nextItem!= NULL) {     elog(ERROR, "3 nextItem = RRNext");       }    
-        if (prevItem->nextItem == RRNext->nextItem && prevItem->nextItem!= NULL) {     elog(ERROR, "2 nextItem = RRNext");       } 
+        if (prevItem->nextItem == RRNext->nextItem && prevItem->nextItem!= NULL) {     elog(ERROR, "3 nextItem = RRNext");       }
+        if (prevItem->nextItem == RRNext->nextItem && prevItem->nextItem!= NULL) {     elog(ERROR, "2 nextItem = RRNext");       }
 */
         RRNext = RRNext->nextItem;
       } else {  // OBA NULL
@@ -205,7 +197,7 @@ Vertex * addToIndex(int32 vertexId, GraphComponentState* state) {
     fromVertex->id = vertexId;
     fromVertex->nextItem = NULL;
     fromVertex->firstItem = fromVertex;
-    fromVertex->lastItem = fromVertex; 
+    fromVertex->lastItem = fromVertex;
   }
   return fromVertex;
 }
@@ -220,13 +212,15 @@ graph_components_step_arr(PG_FUNCTION_ARGS) {
     GraphComponentState *state;
     ArrayType *currentArray;
     int16 elemTypeWidth;
-    bool elemTypeByValue;
     char elemTypeAlignmentCode;
     int currentLength;
     int id;
     int i;
+    bool elemTypeByValue;
     bool *currentNulls;
     Datum *currentVals;
+    Vertex *prevVertex;
+    Vertex *currentVertex;
 
     if (!AggCheckCallContext(fcinfo, &agg_context)) {
       elog(ERROR, "graph_components_step_arr called in non-aggregate context");
@@ -239,19 +233,21 @@ graph_components_step_arr(PG_FUNCTION_ARGS) {
       ctlDistinct.keysize = sizeof(VertexHash);
       ctlDistinct.entrysize = sizeof(Vertex);
       ctlDistinct.hcxt = agg_context;
-      state->vertices = hash_create("Graph vertex", 48, &ctlDistinct, HASH_ELEM | HASH_BLOBS);
+      state->vertices = hash_create("vertex", 48, &ctlDistinct, HASH_ELEM | HASH_BLOBS);
       state->next = NULL;
 
     } else {
       state = (GraphComponentState *) PG_GETARG_POINTER(0);
     }
-
+    if (PG_ARGISNULL(1)) {
+      PG_RETURN_POINTER(state);
+    }
     currentArray = PG_GETARG_ARRAYTYPE_P(1);
+    if (currentArray == NULL) {
+        PG_RETURN_POINTER(state);
+    }
     get_typlenbyvalalign(INT4OID, &elemTypeWidth, &elemTypeByValue, &elemTypeAlignmentCode);
     deconstruct_array(currentArray, INT4OID, elemTypeWidth, elemTypeByValue, elemTypeAlignmentCode, &currentVals, &currentNulls, &currentLength); // O(N)
-
-    Vertex *prevVertex;
-    Vertex *currentVertex; 
 
     for (i = 0; i < currentLength; i++) { // O(N * logN)
       id = DatumGetInt32(currentVals[i]);
@@ -279,7 +275,6 @@ void copyLinkedListToArray(Vertex *arr, Datum* output, int deep, int max) {
 
 /* Policz ilość elementów. Złożoność O(logN) */
 int countLinkedList(Vertex *arr, int deep) {
-
   if (arr == NULL) {
     return 0;
   }
@@ -291,148 +286,122 @@ int countLinkedList(Vertex *arr, int deep) {
   }
   return 1 + countLinkedList(arr->nextItem, ++deep);
 }
-
 /*
-  Datum
-  graph_component_combine(PG_FUNCTION_ARGS)
-  {
-      GraphComponentState *state1;
-      GraphComponentState *state2;
-      MemoryContext agg_context;
-      MemoryContext old_context;
-      Vertex *s, *s2;
+Datum
+graph_component_combine(PG_FUNCTION_ARGS)
+{
+    GraphComponentState *state1;
+    GraphComponentState *state2;
+    MemoryContext agg_context;
+    MemoryContext old_context;
+    Vertex *s, *s2;
 
-      HASH_SEQ_STATUS scan_status;
-      Vertex *entry;
-      Vertex *currentVertex;
-      bool found;
+    HASH_SEQ_STATUS scan_status;
+    Vertex *entry;
+    Vertex *currentVertex;
+    bool found;
 
-      if (!AggCheckCallContext(fcinfo, &agg_context)) {
-        elog(ERROR, "graph_component_combine called in non-aggregate context");
-      }
+    if (!AggCheckCallContext(fcinfo, &agg_context)) {
+      elog(ERROR, "graph_component_combine called in non-aggregate context");
+    }
 
-      state1 = PG_ARGISNULL(0) ? NULL : (GraphComponentState *) PG_GETARG_POINTER(0);
-      state2 = PG_ARGISNULL(1) ? NULL : (GraphComponentState *) PG_GETARG_POINTER(1);
+    state1 = PG_ARGISNULL(0) ? NULL : (GraphComponentState *) PG_GETARG_POINTER(0);
+    state2 = PG_ARGISNULL(1) ? NULL : (GraphComponentState *) PG_GETARG_POINTER(1);
 
-      if (state2 == NULL){
-          int size = hash_get_num_entries(state1->vertices);
-          elog(WARNING, "connect only A: %d", size);
-          PG_RETURN_POINTER(state1);
-      }
-      old_context = MemoryContextSwitchTo(agg_context);
-      if (state1 == NULL)
-      {
-          state1 = (GraphComponentState *) MemoryContextAlloc(agg_context, sizeof(GraphComponentState));
-          HASHCTL ctlDistinct;
-          memset(&ctlDistinct, 100, sizeof(ctlDistinct));
-          ctlDistinct.keysize = sizeof(VertexHash);
-          ctlDistinct.entrysize = sizeof(Vertex);
-          ctlDistinct.hcxt = agg_context;
-          state1->vertices = hash_create("Graph vertex", 48, &ctlDistinct, HASH_ELEM | HASH_BLOBS);
-          state1->next = NULL;
+    if (state2 == NULL) {
+        int size = hash_get_num_entries(state1->vertices);
+        elog(WARNING, "connect only A: %d", size);
+        PG_RETURN_POINTER(state1);
+    }
+    old_context = MemoryContextSwitchTo(agg_context);
+    if (state1 == NULL) {
+        state1 = (GraphComponentState *) MemoryContextAlloc(agg_context, sizeof(GraphComponentState));
+        HASHCTL ctlDistinct;
+        memset(&ctlDistinct, 100, sizeof(ctlDistinct));
+        ctlDistinct.keysize = sizeof(VertexHash);
+        ctlDistinct.entrysize = sizeof(Vertex);
+        ctlDistinct.hcxt = agg_context;
+        state1->vertices = hash_create("Graph vertex", 48, &ctlDistinct, HASH_ELEM | HASH_BLOBS);
+        state1->next = NULL;
 
-          hash_seq_init(&scan_status, state2->vertices);
-          while ((entry = (Vertex*) hash_seq_search(&scan_status)) != NULL) {
-              currentVertex = addToIndex(id, state1);
-              // kopiuj połączenia z entry do currentVertex
-
-              Vertex* link = currentVertex->firstItem;
-              while (link != NULL) {
-                link->lastItem = newLast;
-                link->firstItem = newFirst;
-
-                link = link->nextItem;
-              }  
+        hash_seq_init(&scan_status, state2->vertices);
+        Vertex *prevVertex;
+        int i =0;
+        while ((entry = (Vertex*) hash_seq_search(&scan_status)) != NULL) {
+          currentVertex = addToIndex(entry->id, state1);
+          // kopiuj połączenia z entry do currentVertex
+          if (i++ > 0) {   // połącz z poprzednim w arr
+            mergeToFirst(prevVertex, currentVertex);
           }
-
-          for(s2=state2->keys; s2 != NULL; s2=(hashMap*)(s2->hh.next)) {
-            elog(WARNING, "  user id %d: value %d", s2->id, s2->value);
-            s = (hashMap*)malloc(sizeof(hashMap));
-            if(s == NULL){
-               elog(ERROR, "malloc error!"); 
-            }else{
-              s->value = s2->value;
-              s->connected = NULL;
-              s->id = s2->id;
-              HASH_ADD_INT( state1->keys, id, s );
-            }
-          }
-          MemoryContextSwitchTo(old_context);
-
-          PG_RETURN_POINTER(state1);
-      } else {
-        // state1->inputElementType = state2->inputElementType;
-        // merge the two arrays 
-        for(s2=state2->keys; s2 != NULL; s2=(hashMap*)(s2->hh.next)) {
-            //  elog(WARNING, "  user id %d: value %d", s2->id, s2->value);
-            searchForKey = s2->id;
-            HASH_FIND_INT( state1->keys, &searchForKey, s );// each element is an ID of tag, add +1 for every ID
-
-            if(s == NULL){
-              s = (hashMap*)malloc(sizeof(hashMap));
-              if(s == NULL){
-                 elog(ERROR, "malloc error!"); 
-              }else{
-                s->value = s2->value;
-                s->id = searchForKey;
-                s->connected = NULL;
-                HASH_ADD_INT( state1->keys, id, s );
-              }
-            }else{
-              s->value += s2->value;
-            }
+          prevVertex = currentVertex;
         }
-    //    elog(WARNING, "connect %d + %d = %d", s1, HASH_COUNT(state2->keys), HASH_COUNT(state1->keys) );
         MemoryContextSwitchTo(old_context);
 
         PG_RETURN_POINTER(state1);
+    } else {
+      // merge the two arrays
+      hash_seq_init(&scan_status, state2->vertices);
+      Vertex *prevVertex;
+      int i = 0;
+      while ((entry = (Vertex*) hash_seq_search(&scan_status)) != NULL) {
+        currentVertex = addToIndex(entry->id, state1);
+        // kopiuj połączenia z entry do currentVertex
+        if (i++ > 0) {   // połącz z poprzednim w arr
+          mergeToFirst(prevVertex, currentVertex);
+        }
+        prevVertex = currentVertex;
       }
-  }
+  //    elog(WARNING, "connect %d + %d = %d", s1, HASH_COUNT(state2->keys), HASH_COUNT(state1->keys) );
+      MemoryContextSwitchTo(old_context);
+      PG_RETURN_POINTER(state1);
+    }
+}
 */
-
 Datum graph_components_final(PG_FUNCTION_ARGS) {
   GraphComponentState *state = (GraphComponentState *) PG_GETARG_POINTER(0);
   PG_RETURN_POINTER(state);
 }
 
 /*
-  Funkcja rozpakowująca dane do wierszy. 
+  Funkcja rozpakowująca dane do wierszy.
   Złożoność O(N*logN)
 */
 Datum
 get_connected_components(PG_FUNCTION_ARGS) {
-    GraphComponentState *state = (GraphComponentState *) PG_GETARG_POINTER(0);
+
     FuncCallContext *funcctx;
     MemoryContext oldcontext;
-    ArrayType  * result;
+    ArrayType  *result;
     Vertex *vertexEntry;
-        
+    Datum *output;
+    int size;
+
     if (SRF_IS_FIRSTCALL()) {
         funcctx = SRF_FIRSTCALL_INIT();
         oldcontext = MemoryContextSwitchTo(funcctx->multi_call_memory_ctx);
+        if (PG_ARGISNULL(0)){
+          funcctx->max_calls = 0;
+          funcctx = SRF_PERCALL_SETUP();
+          SRF_RETURN_DONE(funcctx);
+        }
+        GraphComponentState *state = (GraphComponentState *) PG_GETARG_POINTER(0);
         funcctx->user_fctx = (void *) state;
         funcctx->max_calls = hash_get_num_entries(state->vertices);
         hash_seq_init(&state->hash_seq, state->vertices);
-
         MemoryContextSwitchTo(oldcontext);
     }
 
     funcctx = SRF_PERCALL_SETUP();
-    GraphComponentState *input_text = (GraphComponentState *) funcctx->user_fctx;
- 
+    GraphComponentState *input = (GraphComponentState *) funcctx->user_fctx;
+
     if (funcctx->call_cntr < funcctx->max_calls) {
       do {
-        vertexEntry = (Vertex *) hash_seq_search(&input_text->hash_seq);    // O(logN)
-        if (!vertexEntry) {
-          //    hash_seq_term(&input_text->hash_seq);
-          SRF_RETURN_DONE(funcctx);
-          break;
-        }
+        vertexEntry = (Vertex *) hash_seq_search(&input->hash_seq);    // O(logN)
         // pomijamy już wypisane komponenty
       } while (vertexEntry && (vertexEntry->firstItem->nextItem == vertexEntry->firstItem) );   // O(N)
       if (vertexEntry) {
-        int size = countLinkedList(vertexEntry->firstItem, 0);
-        Datum *output = (Datum*) palloc(size * sizeof(Datum));
+        size = countLinkedList(vertexEntry->firstItem, 0);
+        output = (Datum*) palloc(size * sizeof(Datum));
         copyLinkedListToArray(vertexEntry->firstItem, output, 0, size);
         result = construct_array(output, size, INT4OID, 4, true, 'i');
 
@@ -442,13 +411,18 @@ get_connected_components(PG_FUNCTION_ARGS) {
         vertexEntry->firstItem->nextItem = vertexEntry->firstItem;
 
         SRF_RETURN_NEXT(funcctx, PointerGetDatum(result));
+      } else {
+        if (input->vertices != NULL) {
+            input->vertices = NULL;
+            hash_destroy(input->vertices);
+        }
+        SRF_RETURN_DONE(funcctx);
       }
-      hash_seq_term(&input_text->hash_seq);
-
-      SRF_RETURN_DONE(funcctx);
     } else {
-      hash_seq_term(&input_text->hash_seq);
-
+      if (input->vertices) {
+        hash_seq_term(&input->hash_seq);
+        hash_destroy(input->vertices);
+      }
       SRF_RETURN_DONE(funcctx);
     }
 }
